37cb100c2407107984df969b684886ecb2847188
irina.lebedeva <irina.lebedeva@travelline.ru>
2022-03-10 01:22:28 +0300

    Добавляет использование утилиты форматирования даты

--- project/src/components/property-review-item/property-review-item.tsx
+++ project/src/components/property-review-item/property-review-item.tsx
@@ -1,11 +1,7 @@
-import dayjs from 'dayjs';
+import {dateFormat} from 'utils/date-format';
+import {DateFormat} from 'settings/date-format';
 import {getRatingInPercent} from 'utils/get-rating-in-percent';
 import {Review} from 'types/review';
-
-const enum DateFormat {
-  Display = 'MMMM YYYY',
-  Markup = 'YYYY-MM-DD',
-}
 
 type PropertyReviewItemProps = {
   review: Review;
@@ -32,8 +28,8 @@ function PropertyReviewItem({review}: PropertyReviewItemProps): JSX.Element {
         <p className="reviews__text">
           {review.comment}
         </p>
-        <time className="reviews__time" dateTime={dayjs(review.date).format(DateFormat.Markup)}>
-          {dayjs(review.date).format(DateFormat.Display)}
+        <time className="reviews__time" dateTime={dateFormat(review.date, DateFormat.Markup)}>
+          {dateFormat(review.date)}
         </time>
       </div>
     </>
