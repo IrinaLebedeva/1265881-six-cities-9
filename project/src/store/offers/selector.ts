62f40e2f27e7d37c28db7c1c1ae09d2d9904ade0
irina.lebedeva <irina.lebedeva@travelline.ru>
2022-03-16 01:49:13 +0300

    Добавляет мемоизацию селекторов

--- project/src/store/offers/selector.ts
+++ project/src/store/offers/selector.ts
@@ -1,34 +1,44 @@
 import {State} from 'types/state';
 import {City} from 'settings/city';
 import {CityCode} from 'types/city-code';
+import {createSelector} from 'reselect';
+import {Offers} from 'types/offer';
 import {OffersSortCodes} from 'settings/offers-sort-type';
+import {OffersSortTypeKey} from 'types/offers-sort-type-key';
 import {
   sortOffersByPriceAsc,
   sortOffersByPriceDesc,
   sortOffersByRatingDesc
 } from 'utils/sort-offers';
 
-const selectCurrentCityCodeOffers = (state: State) => (
-  state.offersReducer.offers.filter((offer) => offer.city.name === City[state.cityReducer.cityCode as CityCode])
-);
+const getCurrentCityCode = (state: State): CityCode => state.cityReducer.cityCode;
+const getOffers = (state: State): Offers => state.offersReducer.offers;
 
-const selectSortedCurrentCityCodeOffers = (state: State) => {
-  const sortedOffers = selectCurrentCityCodeOffers(state);
-  switch (state.offersReducer.offersSortType) {
-    case OffersSortCodes.PRICE_HIGH_TO_LOW:
-      sortedOffers.sort(sortOffersByPriceDesc);
-      break;
-    case OffersSortCodes.PRICE_LOW_TO_HIGH:
-      sortedOffers.sort(sortOffersByPriceAsc);
-      break;
-    case OffersSortCodes.TOP_RATED_FIRST:
-      sortedOffers.sort(sortOffersByRatingDesc);
-      break;
-    case OffersSortCodes.POPULAR:
-      break;
-  }
-  return sortedOffers;
-};
+const selectCurrentCityCodeOffers = createSelector(
+  [getCurrentCityCode, getOffers], (currentCityCode, offers) => (
+    offers.filter((offer) => offer.city.name === City[currentCityCode as CityCode])
+  ));
+
+const getOffersSortType = (state: State): OffersSortTypeKey => state.offersReducer.offersSortType;
+
+const selectSortedCurrentCityCodeOffers = createSelector(
+  [getOffersSortType, selectCurrentCityCodeOffers],
+  (offersSortType, sortedOffers) => {
+    switch (offersSortType) {
+      case OffersSortCodes.PRICE_HIGH_TO_LOW:
+        sortedOffers.sort(sortOffersByPriceDesc);
+        break;
+      case OffersSortCodes.PRICE_LOW_TO_HIGH:
+        sortedOffers.sort(sortOffersByPriceAsc);
+        break;
+      case OffersSortCodes.TOP_RATED_FIRST:
+        sortedOffers.sort(sortOffersByRatingDesc);
+        break;
+      case OffersSortCodes.POPULAR:
+        break;
+    }
+    return sortedOffers;
+  });
 
 export {
   selectCurrentCityCodeOffers,
