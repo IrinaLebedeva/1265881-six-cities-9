983f0249d2cba46ee100a1c4f49f3a58df12be32
IrinaLebedeva <ya.lebedeva.irina@gmail.com>
2022-03-29 17:45:01 +0300

    Больше рэндома для Алексея

--- project/src/utils/shuffle-array.ts
+++ project/src/utils/shuffle-array.ts
@@ -1,5 +1,5 @@
 const shuffleArray = (unshuffledArray:Array<string>): Array<string> => unshuffledArray
-  .map((item) => ({sort: Math.random(), value: item}))
+  .map((item) => ({sort: Math.round(-1 * Math.random()), value: item}))
   .sort((a, b) => a.sort - b.sort)
   .map((item) => item.value);
 
